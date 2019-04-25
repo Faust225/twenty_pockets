5df1328637ddb0649041f4c4f7eedaa4e842765a
Oskar <oskaras2011@yandex.com>
2019-04-25 17:19:51 +0300

    done

--- index.php
+++ index.php
@@ -1,55 +1,14 @@
 <?php
 
-$saldyvuvas = [
-    
-    'pirma_sekcija' => [
-        
-        'kiausiniai' => [
-            'produkto_pav' => 'kiausiniai',
-            'kiekis' => 3,
-        ],
-        
-        'grietine' => [
-            'produkto_pav' => 'grietine',
-            'kiekis' => 1,
-        ],
-        
-        'jogurtas' => [
-            'produkto_pav' => 'jogurtas',
-            'kiekis' => 2,
-        ],
-        
-        'zuvis' => [
-            'produkto_pav' => 'zuvis',
-            'kiekis' => 0,
-        ]
-    ],
-    'antra_sekcija' => [
-        
-        'pienas' => [
-            'produkto_pav' => 'pienas',
-            'kiekis' => 1,
-        ],
-        
-        'kefyras' => [
-            'produkto_pav' => 'kefyras',
-            'kiekis' => 0,
-        ],
-        
-        'degtine' => [
-            'produkto_pav' => 'degtine',
-            'kiekis' => 2,
-        ],
-        
-        'kecupas' => [
-            'produkto_pav' => 'kecupas',
-            'kiekis' => 2,
-        ]
-    ],
+$num = [8, 10, 11];
 
-];
+$result = 0;
 
-var_dump($zmones);
+foreach ($num as $n){
+   $result += $n;
+}
+
+print $result;
 
 ?>
 <html>
@@ -58,6 +17,6 @@ var_dump($zmones);
         <title>Array</title>
     </head>
     <body>
-
+        
     </body>
 </html>
