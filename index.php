3778e28fbda985644fe82c443167dde93d9c33c0
Oskar <oskaras2011@yandex.com>
2019-04-26 14:38:42 +0300

    Monday, Friday adventures

--- index.php
+++ index.php
@@ -1,14 +1,63 @@
 <?php
 
-$num = [8, 10, 11];
+$bar_adventure = '';
 
-$result = 0;
+$random_result = '';
 
-foreach ($num as $n){
-   $result += $n;
+$result = '';
+
+$day_adventures = [
+    
+  'pirmadienis' => [
+      
+    'week_day' => 'Pirmadienis',
+     
+     'CA' => [
+ 
+         'CA' => 'Paskaita'        
+     ]
+  ],
+   
+   'penktadienis' => [
+        
+     'week_day' => 'Penktadienis',
+       
+      'baras' => [
+          
+          'Paskaita',
+          'viskis',
+          'alus',
+          'degtine'
+          ]
+    ]
+    
+];
+var_dump($day_adventures);
+
+
+foreach($day_adventures as $day => $week_day) {
+    
+    if($day == 'penktadienis') {
+        
+    foreach($week_day as $key => $place_and_act) {
+        
+        $result .= "<li> $place_and_act </li>";
+        
+           //baras                   keys => values
+           foreach($place_and_act as $drinks => $bar) {
+                   
+              
+              // bar size
+              $drink_lenght = count($place_and_act);
+            
+              // bar min and max
+              $random_drink = rand(0, $drink_lenght - 1);
+              
+               $random_friend_drink = rand(0, $drink_lenght - 1);
+             }
+        }
+    }
 }
-
-print $result;
 
 ?>
 <html>
@@ -17,6 +66,11 @@ print $result;
         <title>Array</title>
     </head>
     <body>
-        
+        <ul>
+        <?php print_r($result); ?>
+        </ul>
+           
+        <h1><?php print 'My memories ' . $day_adventures['penktadienis']['baras'][$random_drink];?></h1>  
+        <?php print 'Friend memories ' . $day_adventures['penktadienis']['baras'][$random_friend_drink];?>
     </body>
 </html>
