9c0541f684f18b9c491b7e27e52cee92dd6160df
Oskar <oskaras2011@yandex.com>
2019-05-02 15:40:49 +0300

    Arr indexes without recursion

--- index.php
+++ index.php
@@ -7,16 +7,20 @@
 function slot_run($arr_size = 5) {
 
     $empty_arr = [];
+    $all_text = '';
 
 for($i = 0; $i < $arr_size; $i++) {
     
+    $all_text .= " <p> ";
     for($j = 0; $j < $arr_size; $j++) {
         
         $empty_arr[$i][$j] = rand(0, 1);
+        $all_text .= "<h1> " . $empty_arr[$i][$j] . "<span> [row: $i col: $j] </span>" ." </h1> ";
     }
-    
+    $all_text .= " </p> ";
 }
-  var_dump($empty_arr);
+ // return $empty_arr;
+  print $all_text;
 }
 ?> 
 
@@ -27,10 +31,15 @@ for($i = 0; $i < $arr_size; $i++) {
         <meta charset="utf-8"> 
         <link rel="stylesheet" type="text/css" href="css/style.css"> 
         <style> 
+            
+            h1 {
+                display: inline-block;
+            }
         </style> 
     </head> 
     <body> 
-        <?php slot_run(); ?> 
+        <?php
+         print slot_run();
+         ?> 
     </body> 
-
 </html>
